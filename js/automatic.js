0f857407f160a48f750f675350d13a131b044260
passos2001 <91146306+passos2001@users.noreply.github.com>
2025-11-21 08:22:50 -0500

    Commit 4
    Actualización de semana 17 a semana 20

--- js/automatic.js
+++ js/automatic.js
@@ -1,8 +1,8 @@
 const semanasMap = {
-  semana1: 13,
-  semana2: 14,
-  semana3: 15,
-  semana4: 16
+  semana1: 17,
+  semana2: 18,
+  semana3: 19,
+  semana4: 20
 };
 
 async function cargarSemana(semanaHtmlId, semanaNumero) {
@@ -35,4 +35,3 @@ async function cargarSemana(semanaHtmlId, semanaNumero) {
 Object.entries(semanasMap).forEach(([semanaHtmlId, semanaNumero]) => {
   cargarSemana(semanaHtmlId, semanaNumero);
 });
-
